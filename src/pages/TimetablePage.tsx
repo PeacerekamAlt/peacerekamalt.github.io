2e84c9e01280fca1dbe00c777e5d069102f74d5a
Kamil Masłowiec <kpmaslowiec@gmail.com>
2023-12-28 23:19:22 +0100

    a

--- src/pages/TimetablePage.tsx
+++ src/pages/TimetablePage.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 export const TimetablePage: React.FC = () => {
-
   return (
     <div className="dynamic-content">
       <div className="timetable-section">
-        <img src="/wieliczka-gdow-14-11.png" />
-        <img src="/bochnia-gdow-14-11.png" />
+        <img alt="timetable" src="/wieliczka-gdow-14-11.png" />
+        <img alt="timetable" src="/bochnia-gdow-14-11.png" />
       </div>
     </div>
   );
